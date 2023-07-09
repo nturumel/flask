32034e3e5cd17a172aa7576a02c9e4def7a2c259
Nihar <45964451+nturumel@users.noreply.github.com>
2023-07-09 03:29:45 -0500

    Fixing logging for app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # settings.py
 import logging
+import sys
 from dotenv import load_dotenv
 env = load_dotenv(".env.local")
 from twk_backend.router import app
@@ -8,6 +9,16 @@ from twk_backend.router import app
 # Set up root logger to write messages with level INFO or higher to stdout.
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s', filename="app.log")
+
+# Create a console handler and set its level to INFO
+console_handler = logging.StreamHandler(sys.stdout)
+
+# Create a formatter and add it to the console handler
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# Add the console handler to the root logger
+logging.getLogger('').addHandler(console_handler)
 
 if __name__ == "main":
     app.run(port=5000)  #
