f828c927c618c81555eb5860578f6a685b6dd8ac
Dylan Brookes <dylanbrookes10@gmail.com>
2023-07-09 03:03:51 -0500

    updates

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from dotenv import load_dotenv
+# settings.py
 import logging
+from dotenv import load_dotenv
+env = load_dotenv(".env.local")
+from twk_backend.router import app
+
 
 # Set up root logger to write messages with level INFO or higher to stdout.
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename="app.log")
-
-# settings.py
-env = load_dotenv(".env.local")
-
-from twk_backend.router import app
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s', filename="app.log")
 
 if __name__ == "main":
     app.run(port=5000)  #
